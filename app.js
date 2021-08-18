fa72c195df3fb3fc70c34f77039174e55bd5876c
April Bullard <aprilbullard@Aprils-MacBook.local>
2021-08-18 17:26:24 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "April";
+let lastName = "Bullard";
+var age = "35";
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ",fullName);
+console.log("templateFullName: " ,templateFullName);
 // Exercise 3
+let myStory;
+let city = "San Antonio";
+let passtime = "solving mysteries and coding";
+myStory = `My name is ${fullName}. I live in ${city}.  I enjoy ${passtime}!`;
+console.log(myStory);
